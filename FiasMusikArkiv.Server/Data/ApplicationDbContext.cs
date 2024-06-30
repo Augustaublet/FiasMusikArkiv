19fbb0f8799d8ed61a851134c15f3fe3b3d6c265
AugustAublet <august.aublet@soprasteria.com>
2024-06-30 13:37:46 +0200

    Adding .gitignore to the repository

---
+++ FiasMusikArkiv.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FiasMusikArkiv.Server.Data
+{
+    public class FiasMusikArkivDbContext : DbContext
+    {
+        public FiasMusikArkivDbContext(DbContextOptions<FiasMusikArkivDbContext> options) : base(options) { }
+    }
+}
