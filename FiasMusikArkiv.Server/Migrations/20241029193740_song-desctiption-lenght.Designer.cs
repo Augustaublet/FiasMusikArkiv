64305d22c7fe39895b4ff214e1ffe6aad0f1aed8
AugustAublet <august.aublet@soprasteria.com>
2024-10-29 20:52:27 +0100

    max length to description

--- FiasMusikArkiv.Server/Migrations/20241029193740_song-desctiption-lenght.Designer.cs
+++ FiasMusikArkiv.Server/Migrations/20241029193740_song-desctiption-lenght.Designer.cs
@@ -4,6 +4,7 @@ using FiasMusikArkiv.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FiasMusikArkiv.Server.Migrations
 {
     [DbContext(typeof(FiasMusikArkivDbContext))]
-    partial class FiasMusikArkivDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241029193740_song-desctiption-lenght")]
+    partial class songdesctiptionlenght
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FiasMusikArkiv.Server/Migrations/20241029193740_song-desctiption-lenght.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FiasMusikArkiv.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class songdesctiptionlenght : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Songs",
+                type: "nvarchar(512)",
+                maxLength: 512,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Songs",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(512)",
+                oldMaxLength: 512,
+                oldNullable: true);
+        }
+    }
+}
